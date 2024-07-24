d038b5faab57ff7776f99d91bd5e6d9c7810abbf
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-07-24 23:30:59 +0200

    Add simple cache-blocking loop
    	-	Now there is a simple cache-bocking loop
    	-	Not that performant --> lots of hit misses
    	-	we have to reduce them

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,11 @@
 
 using namespace std ; 
 
-const int N = 1024;
-const int STEPS = 100000;
-const double Du = 0.16;
-const double Dv = 0.08;
-const double F = 0.035;
-const double k = 0.065;
 
 template <typename FLOAT>
 class GS {
 public:
-	int N = 1024;
+	int N = 4096;
 	int STEPS = 1000;
 	FLOAT Du = 0.16;
 	FLOAT Dv = 0.08;
@@ -62,6 +56,34 @@ auto step() {
     v = v_next;
 }
 
+
+auto step_blocking(int BLOCK_SIZE){
+//    int BLOCK_SIZE = 32;
+
+    u_next = u ; 
+    v_next = v ; 
+
+    #pragma omp for schedule(static)
+    for (int ii = 1 ; ii < N-1 ; ii += BLOCK_SIZE){
+        for (int jj = 1 ; jj < N-1 ; jj += BLOCK_SIZE){
+            for (int i = ii; i < min(ii+BLOCK_SIZE, N-1); i+=1){
+		#pragma omp simd
+                for (int j = jj; j < min(jj+BLOCK_SIZE, N-1); j+=1){
+                    double laplacian_u = u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1] - 4 * u[i][j];
+                    double laplacian_v = v[i-1][j] + v[i+1][j] + v[i][j-1] + v[i][j+1] - 4 * v[i][j];
+                    double u_val = u[i][j];
+                    double v_val = v[i][j];
+                    u_next[i][j] = u_val + (Du * laplacian_u - u_val * v_val * v_val + F * (1 - u_val));
+                    v_next[i][j] = v_val + (Dv * laplacian_v + u_val * v_val * v_val - (F + k) * v_val);
+                }
+            }
+        }
+    }
+    u = u_next;
+    v = v_next;
+}
+
+
 void save_grid(const std::vector<std::vector<FLOAT>>& grid, int step) {
     std::ofstream file("output_step_" + std::to_string(step) + ".txt");
     if (file.is_open()) {
@@ -81,45 +103,6 @@ private:
 
 };
 
-
-
-auto initialize(std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& v) {
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (i > N/2-10 && i < N/2+10 && j > N/2-10 && j < N/2+10) {
-                u[i][j] = 0.5;
-                v[i][j] = 0.25;
-            } else {
-                u[i][j] = 1.0;
-                v[i][j] = 0.0;
-            }
-        }
-    }
-}
-
-auto step(std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& v) {
-    std::vector<std::vector<double>> u_next = u;
-    std::vector<std::vector<double>> v_next = v;
-
-//    #pragma omp parallel for collapse(2)
-    #pragma omp parallel for schedule(static) shared(u, v, u_next, v_next)
-    for (int i = 1; i < N - 1; ++i) {
-	#pragma omp simd
-        for (int j = 1; j < N - 1; ++j) {
-            double laplacian_u = u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1] - 4 * u[i][j];
-            double laplacian_v = v[i-1][j] + v[i+1][j] + v[i][j-1] + v[i][j+1] - 4 * v[i][j];
-
-            double u_val = u[i][j];
-            double v_val = v[i][j];
-
-            u_next[i][j] = u_val + (Du * laplacian_u - u_val * v_val * v_val + F * (1 - u_val));
-            v_next[i][j] = v_val + (Dv * laplacian_v + u_val * v_val * v_val - (F + k) * v_val);
-        }
-    }
-
-    u = u_next;
-    v = v_next;
-}
 
 
 void save_grid(const std::vector<std::vector<double>>& grid, int step) {
@@ -144,16 +127,17 @@ int main() {
     using FLOAT = float ; 
 
     GS<FLOAT> gs ;
-    gs.u = vector<vector<FLOAT>>(N, vector<FLOAT>(N, 1.0));
-    gs.v = vector<vector<FLOAT>>(N, vector<FLOAT>(N, 0.0));
+    gs.u = vector<vector<FLOAT>>(gs.N, vector<FLOAT>(gs.N, 1.0));
+    gs.v = vector<vector<FLOAT>>(gs.N, vector<FLOAT>(gs.N, 0.0));
 
     gs.initialize();
-
+    #pragma omp parallel
     for (int step_num = 0; step_num < gs.STEPS; ++step_num) {
         gs.step();
+	//gs.step_blocking(32);
         if (step_num % 1000 == 0) {
             std::cout << "Step: " << step_num << std::endl;
-            gs.save_grid(gs.u, step_num);
+//            gs.save_grid(gs.u, step_num);
         }
     }
 
